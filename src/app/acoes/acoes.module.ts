f1a4552858ffa7fcef0b3a5f682788b85d32949d
André <andre.escocard@hesea.com.br>
2022-09-28 16:57:43 -0300

    chore: create json-run command feat: create feature model acoes feat: add database file to repo

---
+++ src/app/acoes/acoes.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AcoesRoutingModule } from './acoes-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AcoesRoutingModule
+  ]
+})
+export class AcoesModule { }
