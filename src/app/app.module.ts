a8d5c511aa3ea3f773106e3b560f88ff0f89e617
André <andre.escocard@hesea.com.br>
2022-09-28 15:42:50 -0300

    feat: add bootstrap html feat: add router-outlet tag and imports style: add bootstrap css and js in json file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
@@ -8,6 +9,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    RouterModule,
     BrowserModule
   ],
   providers: [],
